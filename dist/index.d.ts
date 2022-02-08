250189c03e9fd93bb07cad7ea619b74088413bd7
Charlie Leduc <ca.leduc@me.com>
2022-02-08 21:24:49 +0100

    first commit

---
+++ dist/index.d.ts
@@ -0,0 +1,21 @@
+export interface i18nJSON {
+    [key: string]: string | undefined;
+}
+export interface i18nTranslation {
+    [lang: string]: i18nJSON | undefined;
+}
+export interface i18nParameters {
+    locale: string;
+    tokens?: any[];
+    count?: number;
+    reversed?: boolean;
+}
+export interface i18nOptions {
+    default: string;
+    locales: i18nTranslation;
+}
+export declare function getLocale(): string;
+export declare function setLocale(locale?: string | null): void;
+export declare function addTranslation(locale: string, translation?: i18nJSON): void;
+export declare function getSupportedLocales(): string[];
+export declare function _t(message: string, params?: i18nParameters): string;
---
+++ dist/index.js
@@ -0,0 +1,69 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports._t = exports.getSupportedLocales = exports.addTranslation = exports.setLocale = exports.getLocale = void 0;
+const _I18NT_LOCALE_KEY_ = 'locale';
+const _I18NT_TRANSLATIONS_ = {};
+const _I18NT_SUPPORTED_LOCALES_ = ['en'];
+function getLocale() {
+    var _a;
+    const defaultLocale = _I18NT_SUPPORTED_LOCALES_[0];
+    var storageLocale = localStorage.getItem(_I18NT_LOCALE_KEY_);
+    var locale = (_a = storageLocale !== null && storageLocale !== void 0 ? storageLocale : window.navigator.language) !== null && _a !== void 0 ? _a : defaultLocale;
+    if (locale === 'default') {
+        locale = window.navigator.language;
+    }
+    if (locale.length > 2) {
+        locale = locale.substr(0, 2);
+    }
+    if (_I18NT_SUPPORTED_LOCALES_.indexOf(locale) > -1) {
+        localStorage.setItem(_I18NT_LOCALE_KEY_, locale);
+        return locale;
+    }
+    localStorage.setItem(_I18NT_LOCALE_KEY_, defaultLocale);
+    return defaultLocale;
+}
+exports.getLocale = getLocale;
+function setLocale(locale) {
+    localStorage.removeItem(_I18NT_LOCALE_KEY_);
+    if ((locale !== null && locale !== void 0 ? locale : '').length > 0) {
+        localStorage.setItem(_I18NT_LOCALE_KEY_, locale !== null && locale !== void 0 ? locale : 'default');
+    }
+}
+exports.setLocale = setLocale;
+function addTranslation(locale, translation) {
+    if (!(locale !== null && locale !== void 0 ? locale : '').length) {
+        return;
+    }
+    _I18NT_SUPPORTED_LOCALES_.push(locale);
+    _I18NT_TRANSLATIONS_[locale] = translation;
+}
+exports.addTranslation = addTranslation;
+function getSupportedLocales() {
+    return _I18NT_SUPPORTED_LOCALES_;
+}
+exports.getSupportedLocales = getSupportedLocales;
+function _t(message, params) {
+    var _a, _b, _c, _d, _e, _f, _g;
+    const locale = (_a = params === null || params === void 0 ? void 0 : params.locale) !== null && _a !== void 0 ? _a : getLocale();
+    const translation = (_b = _I18NT_TRANSLATIONS_[locale]) !== null && _b !== void 0 ? _b : null;
+    if (!translation) {
+        return message;
+    }
+    if ((params === null || params === void 0 ? void 0 : params.reversed) === true) {
+        return (_c = Object.keys(translation).find((key) => translation[key] === message)) !== null && _c !== void 0 ? _c : message;
+    }
+    const tokens = (_d = params === null || params === void 0 ? void 0 : params.tokens) !== null && _d !== void 0 ? _d : [];
+    var output = (_f = (_e = translation[message]) !== null && _e !== void 0 ? _e : message) !== null && _f !== void 0 ? _f : '';
+    if (output && (output === null || output === void 0 ? void 0 : output.indexOf('|')) > -1) {
+        const count = (_g = params === null || params === void 0 ? void 0 : params.count) !== null && _g !== void 0 ? _g : 0;
+        const parts = output.split('|');
+        if (parts.length) {
+            output = count < parts.length ? parts[count].trim() : parts[parts.length - 1].trim();
+        }
+    }
+    for (let i = 0; i < tokens.length; i++) {
+        output = output.replace(/%s/u, tokens[i]);
+    }
+    return output;
+}
+exports._t = _t;
