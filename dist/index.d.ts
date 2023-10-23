ff6490f29f4b430842ff4ff66e7b90c1f3b10e2f
Charlie Leduc <contact@graphique.io>
2023-10-23 13:42:57 +0200

    Version 1.0.3

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -10,12 +10,9 @@ export interface i18nParameters {
     count?: number;
     reversed?: boolean;
 }
-export interface i18nOptions {
-    default: string;
-    locales: i18nTranslation;
-}
 export declare function getLocale(): string;
 export declare function setLocale(locale?: string | null): void;
 export declare function addTranslation(locale: string, translation?: i18nJSON): void;
 export declare function getSupportedLocales(): string[];
+export declare function addSupportedLocale(locale: string): void;
 export declare function _t(message: string, params?: i18nParameters): string;
--- dist/index.js
+++ dist/index.js
@@ -1,32 +1,34 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports._t = exports.getSupportedLocales = exports.addTranslation = exports.setLocale = exports.getLocale = void 0;
-const _I18NT_LOCALE_KEY_ = 'locale';
+exports._t = exports.addSupportedLocale = exports.getSupportedLocales = exports.addTranslation = exports.setLocale = exports.getLocale = void 0;
+const _I18NT_LOCALE_KEY_ = "locale";
 const _I18NT_TRANSLATIONS_ = {};
-const _I18NT_SUPPORTED_LOCALES_ = ['en'];
+const _I18NT_SUPPORTED_LOCALES_ = ["en"];
 function getLocale() {
     var _a;
     const defaultLocale = _I18NT_SUPPORTED_LOCALES_[0];
-    let storageLocale = localStorage.getItem(_I18NT_LOCALE_KEY_);
+    const storageLocale = localStorage.getItem(_I18NT_LOCALE_KEY_);
     let locale = (_a = storageLocale !== null && storageLocale !== void 0 ? storageLocale : window.navigator.language) !== null && _a !== void 0 ? _a : defaultLocale;
-    if (locale === 'default') {
+    if (locale === "default") {
         locale = window.navigator.language;
     }
     if (locale.length > 2) {
-        locale = locale.substr(0, 2);
+        locale = locale.substring(0, 2);
     }
-    return _I18NT_SUPPORTED_LOCALES_.indexOf(locale) > -1 ? locale : defaultLocale;
+    return _I18NT_SUPPORTED_LOCALES_.indexOf(locale) > -1
+        ? locale
+        : defaultLocale;
 }
 exports.getLocale = getLocale;
 function setLocale(locale) {
     localStorage.removeItem(_I18NT_LOCALE_KEY_);
-    if ((locale !== null && locale !== void 0 ? locale : '').length > 0) {
-        localStorage.setItem(_I18NT_LOCALE_KEY_, locale !== null && locale !== void 0 ? locale : 'default');
+    if ((locale !== null && locale !== void 0 ? locale : "").length > 0) {
+        localStorage.setItem(_I18NT_LOCALE_KEY_, locale !== null && locale !== void 0 ? locale : "default");
     }
 }
 exports.setLocale = setLocale;
 function addTranslation(locale, translation) {
-    if (!(locale !== null && locale !== void 0 ? locale : '').length) {
+    if (!(locale !== null && locale !== void 0 ? locale : "").length) {
         return;
     }
     if (_I18NT_SUPPORTED_LOCALES_.indexOf(locale) < 0) {
@@ -39,6 +41,12 @@ function getSupportedLocales() {
     return _I18NT_SUPPORTED_LOCALES_;
 }
 exports.getSupportedLocales = getSupportedLocales;
+function addSupportedLocale(locale) {
+    if (_I18NT_SUPPORTED_LOCALES_.indexOf(locale) < 0) {
+        _I18NT_SUPPORTED_LOCALES_.push(locale);
+    }
+}
+exports.addSupportedLocale = addSupportedLocale;
 function _t(message, params) {
     var _a, _b, _c, _d, _e, _f, _g;
     const locale = (_a = params === null || params === void 0 ? void 0 : params.locale) !== null && _a !== void 0 ? _a : getLocale();
@@ -47,15 +55,18 @@ function _t(message, params) {
         return message;
     }
     if ((params === null || params === void 0 ? void 0 : params.reversed) === true) {
-        return (_c = Object.keys(translation).find((key) => translation[key] === message)) !== null && _c !== void 0 ? _c : message;
+        return ((_c = Object.keys(translation).find((key) => translation[key] === message)) !== null && _c !== void 0 ? _c : message);
     }
     const tokens = (_d = params === null || params === void 0 ? void 0 : params.tokens) !== null && _d !== void 0 ? _d : [];
-    var output = (_f = (_e = translation[message]) !== null && _e !== void 0 ? _e : message) !== null && _f !== void 0 ? _f : '';
-    if (output && (output === null || output === void 0 ? void 0 : output.indexOf('|')) > -1) {
+    let output = (_f = (_e = translation[message]) !== null && _e !== void 0 ? _e : message) !== null && _f !== void 0 ? _f : "";
+    if (output && (output === null || output === void 0 ? void 0 : output.indexOf("|")) > -1) {
         const count = (_g = params === null || params === void 0 ? void 0 : params.count) !== null && _g !== void 0 ? _g : 0;
-        const parts = output.split('|');
+        const parts = output.split("|");
         if (parts.length) {
-            output = count < parts.length ? parts[count].trim() : parts[parts.length - 1].trim();
+            output =
+                count < parts.length
+                    ? parts[count].trim()
+                    : parts[parts.length - 1].trim();
         }
     }
     for (let i = 0; i < tokens.length; i++) {
